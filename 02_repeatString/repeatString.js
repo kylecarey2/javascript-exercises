73491947809d01e1e1059bae6130e40bc9d39f0d
Kyle Carey <135062060+kylecarey2@users.noreply.github.com>
2024-07-14 16:38:11 -0400

    Completed 02_repeatString

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,12 @@
-const repeatString = function() {
-
+const repeatString = function(text, repeatInt) {
+    if (repeatInt < 0) {
+        return "ERROR";
+    }
+    let output = '';
+    for (i = 0; i < repeatInt; i++) {
+        output += text;
+    }
+    return output;
 };
 
 // Do not edit below this line
