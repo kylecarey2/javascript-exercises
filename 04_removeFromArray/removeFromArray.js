64567da3ce79e46e7cee8a029e924f3211473eb6
Kyle Carey <135062060+kylecarey2@users.noreply.github.com>
2024-07-14 17:31:58 -0400

    Completed 04_removeFromArray

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,10 @@
-const removeFromArray = function() {
-};
+const removeFromArray = function(array, ...remove) {
+    const newArray = [];
+    array.forEach(value => {
+        if (!remove.includes(value)) newArray.push(value);
+    });
+    return newArray;
+}
 
 // Do not edit below this line
 module.exports = removeFromArray;
